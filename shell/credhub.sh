7caa3ce856fd0c6d8bdcfb35368757c8971c7591
Brad Starkenberg <brad@starkenberg.net>
2018-10-09 22:17:49 -0400

    Change bash shebang statement

--- shell/credhub.sh
+++ shell/credhub.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 cf cs credhub default event-db -c '{"username": "username","password":"password","url":"jdbc:mysql://192.168.8.38:3306/cf_0e979a96_bca6_4a02_a389_7cca40336700"}'
